a4474e91b781a20e360d14c33d4bf2962d759b72
Loci_Natit <l_natit@outlook.com>
2025-02-18 21:47:47 +0800

    *Sign

--- src/main/java/com/lnatit/ccw/block/entity/SugarRefineryBlockEntity.java
+++ src/main/java/com/lnatit/ccw/block/entity/SugarRefineryBlockEntity.java
@@ -5,20 +5,52 @@ import com.lnatit.ccw.menu.SugarRefineryMenu;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.items.ItemStackHandler;
 
 public class SugarRefineryBlockEntity extends BaseContainerBlockEntity
 {
     public static final Component DEFAULT_NAME = Component.translatable("container.sugar_refinery");
+    public static final int REFINE_TIME = 100;
+
+    ItemStackHandler inventory = new ItemStackHandler(8);
+    int refineTime;
+    long finishTime = 0;
+    boolean changed = true;
+
 
     public SugarRefineryBlockEntity(BlockPos pos, BlockState blockState) {
         super(BlockRegistry.SUGAR_REFINERY_BETYPE.get(), pos, blockState);
+    }
+
+    @Override
+    public void setChanged() {
+        this.changed = true;
+        super.setChanged();
+    }
+
+    public void tick(Level level) {
+        // TODO we need to consider the skipped ticks!
+        long delta = finishTime - level.getGameTime();
+        if (this.changed) {
+            if (this.isRefineable()) {
+                // start: update finishTime
+            }
+        }
+        else {
+            if (delta % REFINE_TIME == 0) {
+                // update the results
+            }
+        }
+    }
+
+    private boolean isRefineable() {
+
     }
 
     public static void serverTick(Level level, BlockPos pos, BlockState state, SugarRefineryBlockEntity blockEntity) {
@@ -37,7 +69,7 @@ public class SugarRefineryBlockEntity extends BaseContainerBlockEntity
 
     @Override
     protected void setItems(NonNullList<ItemStack> items) {
-
+        // Is this really usefull?
     }
 
     @Override
