99164e5b4ed9daf3c9db12654d3880669de7b2f6
Loci_Natit <l_natit@outlook.com>
2025-02-20 15:56:37 +0800

    Finish tick logic

--- src/main/java/com/lnatit/ccw/block/entity/SugarRefineryBlockEntity.java
+++ src/main/java/com/lnatit/ccw/block/entity/SugarRefineryBlockEntity.java
@@ -20,14 +20,12 @@ public class SugarRefineryBlockEntity extends BlockEntity implements MenuProvide
 {
     public static final Component DEFAULT_NAME = Component.translatable("container.sugar_refinery");
     public static final int REFINE_TIME = 100;
+    public static final long MASK = 0xFFFF_FFFF_0000_0000L;
 
     ItemStackHandler inventory = new Contents();
-    int refineTime;
     int matchTime = 0;
     long startTime = 0;
-    long finishTime = 0;
     boolean changed = true;
-
 
     public SugarRefineryBlockEntity(BlockPos pos, BlockState blockState) {
         super(BlockRegistry.SUGAR_REFINERY_BETYPE.get(), pos, blockState);
@@ -40,55 +38,47 @@ public class SugarRefineryBlockEntity extends BlockEntity implements MenuProvide
     }
 
     public void tick(Level level) {
-        // TODO we need to consider the skipped ticks!
-        long delta = finishTime - level.getGameTime();
-        if (this.changed) {
-            if (this.isRefineable()) {
-                // start: update finishTime
-            }
-        }
-        else {
-            if (delta % REFINE_TIME == 0) {
-                // update the results
-            }
-        }
-
         // RecipeHolder will match the recipe & store max craft times, and update the data accrodingly
         // BE will store nearest finish time, check it on each tick and update if needed
         // Each time when the contents is changed by external operations (other than recipe update)
         // will trigger a rematch of RecipeHolder
-        delta = level.getGameTime() - this.startTime;
-        if (this.changed)
-        {
-            this.matchRecipe();
-        } else {
-            if (delta >= REFINE_TIME) {
-                int maxTime = toInt(delta / REFINE_TIME);
-                if (maxTime <= matchTime) {
-                    matchTime -= maxTime;
-                    startTime += maxTime * REFINE_TIME;
-                    // generate the outputs
-                } else {
-
+        long delta = level.getGameTime() - this.startTime;
+        if (this.changed) {
+            this.matchTime = matchRecipe();
+            if (this.matchTime > 0) {
+                this.startTime = level.getGameTime();
+            }
+        }
+        else {
+            if (matchTime > 0 && delta >= REFINE_TIME) {
+                int times = toInt(delta / REFINE_TIME);
+                if (times > matchTime) {
+                    times = this.matchTime;
                 }
+                this.matchTime -= times;
+                this.startTime += (long) times * REFINE_TIME;
+                genOutputs(times);
             }
         }
     }
 
-    private void matchRecipe() {
-
+    private int matchRecipe() {
+        return 0;
     }
 
-    private int toInt(long value) {
-
-    }
-
-    private boolean isRefineable() {
+    private void genOutputs(int times) {
 
     }
 
     public static void serverTick(Level level, BlockPos pos, BlockState state, SugarRefineryBlockEntity blockEntity) {
 
+    }
+
+    private static int toInt(long value) {
+        if ((value & MASK) != 0) {
+            return value > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        }
+        return (int) value;
     }
 
     public ItemStackHandler getInventory() {
@@ -114,7 +104,8 @@ public class SugarRefineryBlockEntity extends BlockEntity implements MenuProvide
     /**
      * milk, sugar, main, suppl, output, misc1, misc2, misc3
      */
-    public class Contents extends ItemStackHandler {
+    public class Contents extends ItemStackHandler
+    {
         public Contents() {
             super(8);
         }
