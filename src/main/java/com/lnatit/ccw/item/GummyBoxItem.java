f67b2b9c4a016d7e48fb770cf77d35166d092bde
Loci_Natit <l_natit@outlook.com>
2025-04-24 16:40:10 +0800

    Merge datagen files after src

---
+++ src/main/java/com/lnatit/ccw/item/GummyBoxItem.java
@@ -0,0 +1,10 @@
+package com.lnatit.ccw.item;
+
+import net.minecraft.world.item.Item;
+
+public class GummyBoxItem extends Item
+{
+    public GummyBoxItem(Properties properties) {
+        super(properties);
+    }
+}
