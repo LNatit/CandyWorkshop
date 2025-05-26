edffe2d9f4d048b5fe4c5385461fddf8f08b98be
Loci_Natit <l_natit@outlook.com>
2025-05-26 22:27:41 +0800

    Finally

--- src/main/java/com/lnatit/ccw/compat/ModConstants.java
+++ src/main/java/com/lnatit/ccw/compat/ModConstants.java
@@ -2,6 +2,8 @@ package com.lnatit.ccw.compat;
 
 import com.lnatit.ccw.CandyWorkshop;
 import com.lnatit.ccw.menu.client.SugarRefineryScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -10,9 +12,9 @@ public class ModConstants
 {
     public static final Component TITLE = Component.translatable("compat.ccw.rei.title");
 
-    public static final ResourceLocation BACKGROUND = ResourceLocation.fromNamespaceAndPath(
-            CandyWorkshop.MODID,
-            "textures/gui/sprites/compat/refining.png"
-    );
-    public static final TextureAtlasSprite ANIMATION_SPRITE = SugarRefineryScreen.ANIMATION;
+    public static final TextureAtlasSprite BACKGROUND_SPRITE =
+            Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(
+                    ResourceLocation.fromNamespaceAndPath(CandyWorkshop.MODID, "gui/compat/refining")
+            );
+    public static final TextureAtlasSprite ANIMATION_SPRITE = SugarRefineryScreen.ANIMATION_SPRITE;
 }
--- src/main/java/com/lnatit/ccw/menu/client/SugarRefineryScreen.java
+++ src/main/java/com/lnatit/ccw/menu/client/SugarRefineryScreen.java
@@ -8,24 +8,25 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraftforge.client.gui.widget.ExtendedButton;
 
-public class SugarRefineryScreen extends AbstractContainerScreen<SugarRefineryMenu> {
+public class SugarRefineryScreen extends AbstractContainerScreen<SugarRefineryMenu>
+{
     public static final ResourceLocation BACKGROUND_LOCATION =
             ResourceLocation.fromNamespaceAndPath(CandyWorkshop.MODID, "textures/gui/container/sugar_refinery.png");
-    public static final ResourceLocation ANIMATION_SPRITE =
-            ResourceLocation.fromNamespaceAndPath(CandyWorkshop.MODID, "textures/gui/sprites/container/sugar_refinery/stirring.png");
-    public static final ResourceLocation PROGRESS_LOCATION =
-            ResourceLocation.fromNamespaceAndPath(CandyWorkshop.MODID, "textures/gui/sprites/container/sugar_refinery/progress.png");
-    public static final TextureAtlasSprite ANIMATION = Minecraft.getInstance().getTextureAtlas(
-            InventoryMenu.BLOCK_ATLAS).apply(
-            ResourceLocation.fromNamespaceAndPath(CandyWorkshop.MODID, "textures/gui/sprites/container/sugar_refinery/stirring.png")
-    );
+    public static final TextureAtlasSprite PROGRESS_SPRITE =
+            Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(
+                    ResourceLocation.fromNamespaceAndPath(CandyWorkshop.MODID, "gui/container/sugar_refinery/progress")
+            );
+    public static final TextureAtlasSprite ANIMATION_SPRITE =
+            Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(
+                    ResourceLocation.fromNamespaceAndPath(CandyWorkshop.MODID, "gui/container/sugar_refinery/stirring")
+            );
     public static final Tooltip PAUSE = Tooltip.create(Component.translatable("container.sugar_refinery.pause"));
     public static final Tooltip START = Tooltip.create(Component.translatable("container.sugar_refinery.start"));
     public static final int WIDTH = 176;
@@ -46,7 +47,8 @@ public class SugarRefineryScreen extends AbstractContainerScreen<SugarRefineryMe
         Button switcher = new InvisibleButton(
                 this.leftPos + 70,
                 this.topPos + 18,
-                this::onIconPress);
+                this::onIconPress
+        );
         switcher.setTooltip(this.menu.getProgress() >= 0 ? PAUSE : START);
         this.addRenderableWidget(switcher);
     }
@@ -71,23 +73,26 @@ public class SugarRefineryScreen extends AbstractContainerScreen<SugarRefineryMe
 
         int progress = this.menu.getProgress();
 
-        if (progress > 0)
+        if (progress > 0) {
             guiGraphics.blit(
-                    ANIMATION_SPRITE,
                     this.leftPos + 72,
                     this.topPos + 22,
-                    0, 0,
-                    28, 19
+                    0,
+                    28, 19,
+                    ANIMATION_SPRITE
             );
-        else progress = 0;
+        }
+        else {
+            progress = 0;
+        }
 
         progress = -15 * progress / SugarRefining.REFINE_TIME;
         guiGraphics.blit(
-                PROGRESS_LOCATION,
                 this.leftPos + 72 - progress,
                 this.topPos + 41,
-                0, 0,
-                28 + progress * 2, 1
+                0,
+                28 + progress * 2, 1,
+                PROGRESS_SPRITE
         );
     }
 
