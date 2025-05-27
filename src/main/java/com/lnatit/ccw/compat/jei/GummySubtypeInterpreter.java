6295fb75f3f45191e9f4626914df97108dfca5fa
Loci_Natit <l_natit@outlook.com>
2025-05-27 09:34:51 +0800

    Fix subtype

--- src/main/java/com/lnatit/ccw/compat/jei/GummySubtypeInterpreter.java
+++ src/main/java/com/lnatit/ccw/compat/jei/GummySubtypeInterpreter.java
@@ -1,36 +1,40 @@
 package com.lnatit.ccw.compat.jei;
 
+import com.lnatit.ccw.item.ItemRegistry;
 import com.lnatit.ccw.item.sugaring.Sugar;
+import com.lnatit.ccw.item.sugaring.SugarContents;
 import com.lnatit.ccw.item.sugaring.SugarUtils;
 import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
+import mezz.jei.api.ingredients.subtypes.ISubtypeInterpreter;
 import mezz.jei.api.ingredients.subtypes.UidContext;
+import net.minecraft.core.Holder;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
-public class GummySubtypeInterpreter implements IIngredientSubtypeInterpreter<ItemStack>
+import java.util.Optional;
+
+public class GummySubtypeInterpreter implements ISubtypeInterpreter<ItemStack>
 {
     public static final GummySubtypeInterpreter INSTANCE = new GummySubtypeInterpreter();
 
     @Override
-    public String apply(ItemStack ingredient, UidContext context) {
-        if (!ingredient.hasTag()) {
-            return IIngredientSubtypeInterpreter.NONE;
+    public @Nullable Object getSubtypeData(ItemStack ingredient, UidContext context) {
+        return ingredient.get(ItemRegistry.SUGAR_CONTENTS_DCTYPE);
+    }
+
+    @Override
+    public String getLegacyStringSubtypeInfo(ItemStack ingredient, UidContext context) {
+        SugarContents sugarContents = ingredient.get(ItemRegistry.SUGAR_CONTENTS_DCTYPE);
+        if (sugarContents == null) {
+            return "";
         }
 
-        CompoundTag sugarContents = SugarUtils.getSugarContents(ingredient);
-        StringBuilder stringBuilder = new StringBuilder();
-
-        if (sugarContents.contains(SugarUtils.TAG_SUGAR, CompoundTag.TAG_STRING)) {
-            stringBuilder.append(sugarContents.getString(SugarUtils.TAG_SUGAR));
-        }
-
-        if (sugarContents.contains(SugarUtils.TAG_FLAVOR, CompoundTag.TAG_STRING)) {
-            stringBuilder.append(sugarContents.getString(SugarUtils.TAG_FLAVOR));
-        }
-        else {
-            stringBuilder.append(Sugar.Flavor.toName(Sugar.Flavor.ORIGINAL));
-        }
-
-        return stringBuilder.toString();
+        Optional<Holder<Sugar>> sugar = sugarContents.sugar();
+        return String.format(
+                "%s[%s]",
+                sugar.map(sugarHolder -> sugarHolder.value().name()).orElse("vanilla"),
+                sugarContents.flavor().name()
+        );
     }
 }
