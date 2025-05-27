99fb9d44bb488d492687588ec3dc973896758448
Loci_Natit <l_natit@outlook.com>
2025-05-27 10:28:30 +0800

    bump version to 1.1.2

--- src/main/java/com/lnatit/ccw/item/sugaring/Sugar.java
+++ src/main/java/com/lnatit/ccw/item/sugaring/Sugar.java
@@ -15,7 +15,6 @@ import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.neoforged.neoforge.network.codec.NeoForgeStreamCodecs;
