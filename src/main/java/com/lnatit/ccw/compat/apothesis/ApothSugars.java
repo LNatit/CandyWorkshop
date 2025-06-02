02a130f295ffd404feca3d6b0cf3e7ff229cdfa7
Loci <l_natit@outlook.com>
2025-06-02 10:55:59 +0800

    Fix recipe issue

--- src/main/java/com/lnatit/ccw/compat/apothesis/ApothSugars.java
+++ src/main/java/com/lnatit/ccw/compat/apothesis/ApothSugars.java
@@ -9,18 +9,18 @@ import net.minecraft.world.item.Items;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class ApothSugars {
-    public static final DeferredHolder<Sugar, SingleEffectSugar> BLEEDING = Sugars.registerSingle(
-            "bleeding",
-            builder -> builder
-                    .withEffect(ALObjects.MobEffects.BLEEDING)
-                    .build()
-    );
-
-    public static void init() {
-        SugarRefining.addCustomBlendProviders(ApothSugars::addApothBlends);
-    }
-
-    private static void addApothBlends(SugarRefining.Builder builder) {
-        builder.addEndBlend(BLEEDING, Items.WOODEN_SWORD);
-    }
+//    public static final DeferredHolder<Sugar, SingleEffectSugar> BLEEDING = Sugars.registerSingle(
+//            "bleeding",
+//            builder -> builder
+//                    .withEffect(ALObjects.MobEffects.BLEEDING)
+//                    .build()
+//    );
+//
+//    public static void init() {
+//        SugarRefining.addCustomBlendProviders(ApothSugars::addApothBlends);
+//    }
+//
+//    private static void addApothBlends(SugarRefining.Builder builder) {
+//        builder.addEndBlend(BLEEDING, Items.WOODEN_SWORD);
+//    }
 }
--- src/main/java/com/lnatit/ccw/datagen/ModRecipeProvider.java
+++ src/main/java/com/lnatit/ccw/datagen/ModRecipeProvider.java
@@ -98,13 +98,13 @@ public class ModRecipeProvider extends RecipeProvider
             .unlockedBy("has_plain_drawer_table", has(ItemRegistry.PLAIN_DRAWER_TABLE))
             .save(output);
 
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.BREWING, ItemRegistry.NETHER_SUGAR)
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.BREWING, ItemRegistry.NETHER_SUGAR, 8)
             .requires(Items.NETHER_WART)
             .requires(Items.SUGAR, 8)
             .unlockedBy("has_milk_carton", has(ItemRegistry.CARTON_MILK_TAG))
             .save(output);
 
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.BREWING, ItemRegistry.ENDER_SUGAR)
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.BREWING, ItemRegistry.ENDER_SUGAR, 8)
             .requires(Items.DRAGON_BREATH)
             .requires(Items.SUGAR, 8)
             .unlockedBy("has_milk_carton", has(ItemRegistry.CARTON_MILK_TAG))
