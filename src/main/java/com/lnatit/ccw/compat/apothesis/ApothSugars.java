1a0f681977d2453000ff614036248fc3d117e02f
Loci_Natit <l_natit@outlook.com>
2025-06-28 11:25:49 +0800

    Add Blends & some textures

--- src/main/java/com/lnatit/ccw/compat/apothesis/ApothSugars.java
+++ src/main/java/com/lnatit/ccw/compat/apothesis/ApothSugars.java
@@ -7,24 +7,6 @@ import dev.shadowsoffire.apothic_attributes.api.ALObjects;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class ApothSugars {
-    public static final DeferredHolder<Sugar, SingleEffectSugar> KNOWLEDGE = Sugars.registerSingle(
-            "knowledge",
-            builder -> builder
-                    .withEffect(ALObjects.MobEffects.KNOWLEDGE)
-                    .build()
-    );
-    public static final DeferredHolder<Sugar, SingleEffectSugar> VITALITY = Sugars.registerSingle(
-            "vitality",
-            builder -> builder
-                    .withEffect(ALObjects.MobEffects.VITALITY)
-                    .build()
-    );
-    public static final DeferredHolder<Sugar, SingleEffectSugar> SUNDERING = Sugars.registerSingle(
-            "sundering",
-            builder -> builder
-                    .withEffect(ALObjects.MobEffects.SUNDERING)
-                    .build()
-    );
     public static final DeferredHolder<Sugar, SingleEffectSugar> BLEEDING = Sugars.registerSingle(
             "bleeding",
             builder -> builder
@@ -35,6 +17,24 @@ public class ApothSugars {
             "grievous",
             builder -> builder
                     .withEffect(ALObjects.MobEffects.GRIEVOUS)
+                    .build()
+    );
+    public static final DeferredHolder<Sugar, SingleEffectSugar> KNOWLEDGE = Sugars.registerSingle(
+            "knowledge",
+            builder -> builder
+                    .withEffect(ALObjects.MobEffects.KNOWLEDGE)
+                    .build()
+    );
+    public static final DeferredHolder<Sugar, SingleEffectSugar> SUNDERING = Sugars.registerSingle(
+            "sundering",
+            builder -> builder
+                    .withEffect(ALObjects.MobEffects.SUNDERING)
+                    .build()
+    );
+    public static final DeferredHolder<Sugar, SingleEffectSugar> VITALITY = Sugars.registerSingle(
+            "vitality",
+            builder -> builder
+                    .withEffect(ALObjects.MobEffects.VITALITY)
                     .build()
     );
 
