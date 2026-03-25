48fafd4fb65b1db202ba228676b2d32c1532b9d1
Loci_Natit <l_natit@outlook.com>
2026-03-25 11:12:43 +0800

    update gradle structure

--- src/youkaisFeasts/java/com/lnatit/ccw/compat/youkaisfeasts/YoukaisFeastsCompats.java
+++ src/youkaisFeasts/java/com/lnatit/ccw/compat/youkaisfeasts/YoukaisFeastsCompats.java
@@ -1,7 +1,16 @@
 package com.lnatit.ccw.compat.youkaisfeasts;
 
+import com.lnatit.ccw.item.sugaring.Sugar;
+import com.lnatit.ccw.item.sugaring.Sugars;
+import dev.xkmc.youkaishomecoming.init.food.YHTea;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.neoforged.neoforge.registries.DeferredHolder;
+
 public interface YoukaisFeastsCompats
 {
+    DeferredHolder<Sugar, Sugar> GREEN_TEA = Sugars.SUGARS.register("green_tea",
+                                                                    () -> new Sugar(Sugar.Type.OVERWORLD,
+                                                                                    Ingredient.of(YHTea.GREEN.leaves)));
 
     static void init() {}
 }
